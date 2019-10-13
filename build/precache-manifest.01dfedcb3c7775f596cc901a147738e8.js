0c0d3ce83de866a99480321bf5ca37617edebb03
darklordace <firdaus.alghifari@gmail.com>
2019-10-13 12:12:58 +0700

    new folder structure

---
+++ build/precache-manifest.01dfedcb3c7775f596cc901a147738e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "915036fe1337d8036d25528ff0aa7ff2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0633d761780b7c5de5ac",
+    "url": "/static/css/main.3c1ac498.chunk.css"
+  },
+  {
+    "revision": "1400c5c46300f5bb5b7f",
+    "url": "/static/js/2.0ab11546.chunk.js"
+  },
+  {
+    "revision": "0633d761780b7c5de5ac",
+    "url": "/static/js/main.a57b9230.chunk.js"
+  },
+  {
+    "revision": "9e49d09689ff1de4f3fe",
+    "url": "/static/js/runtime~main.2ce23e4f.js"
+  }
+]);
